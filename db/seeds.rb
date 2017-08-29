626714fcfa75aab609f86015164d5f6c8bf1efa9
DonutJr <cyrus.younessi@gmail.com>
2017-08-28 21:29:02 -0500

    8-28

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# contact = Contact.new(
+#                       first_name: "roci",
+#                       last_name: "cifrian",
+#                       email: "roci@gmail.com",
+#                       phone_number: "305-555-6072")
+
+# contact.save
+
